683e77f69d9936b74d79d0bc74c25d597c449a76
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-02 15:44:16 -0300

    Adicionado metade do layout

---
+++ app/src/main/java/ex3botoes/fiap/com/br/a2tina_android_ex03_botoes/MainActivity.java
@@ -0,0 +1,13 @@
+package ex3botoes.fiap.com.br.a2tina_android_ex03_botoes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
